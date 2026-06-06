1063080775e7b50a77691bc5dce616f53aedeafc
RodrigoGonzalez90 <rodrigo.gonzalez_1990@hotmail.com>
2026-06-06 11:28:45 -0300

    feat: agrega items del scraper

---
+++ src/price_manager/scraper/items.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import scrapy
+
+
+class StarProductItem(scrapy.Item):
+    query = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
+    image_url = scrapy.Field()
+    price_ars = scrapy.Field()
+    payment_options = scrapy.Field()
+    description = scrapy.Field()
+    match_score = scrapy.Field()
